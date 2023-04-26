8336eff8ada84f961bde94d31968cbf772167d86
stephsteph123 <114449532+stephsteph123@users.noreply.github.com>
2023-04-26 14:35:43 -0400

    logout functionality added

--- express_server.js
+++ express_server.js
@@ -1,31 +1,42 @@
+//modules
 const express = require("express");
 const app = express();
 const PORT = 3000;
 const bodyParser = require("body-parser");
+const cookieSession = require('cookie-session');
 const {
   roomsDatabase,
   camerasDatabase,
   usersDatabase,
 } = require("./database");
 
+//middlewear
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.render("login_page");
-});
-
+app.use(cookieSession({
+  name: 'session', 
+  keys: ['secret key'],
+  saveUninitialized: true
+}));
+app.use((req, res, next) => {
+  console.log("sessionListner", req.session)
+  next()
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 //get routes
+app.get("/", (req, res) => {
+  res.render("login_page");
+});
+
 app.get("/login", (req, res) => {
   res.render("login_page");
 });
 
 app.get("/dashboard", (req, res) => {
+
   res.render("dashboard_page");
 });
 
@@ -43,13 +54,17 @@ app.post("/login", (req, res) => {
       userId= usersDatabase[id]["id"];
     }
   }
-  console.log(userId)
   if (userId === "") {
     return res.status(403).send('Please Confirm You Have Entered The Correct Username And Password');
   }
   if(password !== usersDatabase[userId]["password"]){
     return res.status(403).send('Please Confirm You Have Entered The Correct Username And Password');
   }
-
+  req.session.userId = userId;
   res.redirect("/dashboard");
+  });
+
+  app.post("/logout", (req, res) => {
+    delete req.session.userId;
+    res.redirect("/login");
   });
